5778fd7f96f565dd8cb97ee0402e23d9eb1492f3
raynald <raynaldchung@gmail.com>
2015-01-15 17:50:32 +0100

    rev

--- name_repositories.py
+++ name_repositories.py
@@ -14,31 +14,43 @@ class RepositoryCreation():
     def CheckOut(self, org_name):
         if not os.path.exists(org_name):
             os.makedirs(org_name)
-        self.login = "oecyc"
-        self.password = "eth123456"
+        self.login = ""
+        self.password = ""
         self.g = Github(self.login, self.password)
         self.org = self.g.get_organization(org_name)
-        self.repos_poll = self.org.get_repos()
-        for repos in self.repos_poll:
+        for repos in self.org.get_repos():
             workbook = xlsxwriter.Workbook(org_name + '/' + repos.name + '.xlsx')
             worksheet = workbook.add_worksheet()
-            owner_name = repos.owner.name
+            owner_login = repos.owner.login
             count = 0
             user_poll = repos.get_stats_contributors()
+            while user_poll == None:
+                user_poll = repos.get_stats_contributors()
+            print "Done!", repos.name
             for stat in user_poll:
                 count += 1
                 worksheet.write('A' + str(count), stat.author.login)
-                if stat.author.name == owner_name:
+                if stat.author.login == owner_login:
                     worksheet.write('B' + str(count), 'owner')
                 else:
-                    worksheet.write('B' + str(count), 'non-owner')
+                    worksheet.write('B' + str(count), 'collaborator')
                 worksheet.write('C' + str(count), stat.author.email)
                 weeks_commits = stat.weeks
                 col = 4
+                old_month = 0
+                old_year = 0
+                old_c = 0
                 for commits in weeks_commits:
-                    col_num = self.Convert26(col)
-                    col += 1
-                    worksheet.write(col_num + str(count), str(commits.a) + ',' + str(commits.c) + ',' + str(commits.d) + ',' + str(commits.w))
+                    if commits.w.month == old_month:
+                        old_c += commits.c
+                    else:
+                        if old_month > 0:
+                            col_num = self.Convert26(col)
+                            col += 1
+                            worksheet.write(col_num + str(count), str(old_year) + '/' + str(old_month) + ':' + str(old_c))
+                        old_month = commits.w.month
+                        old_year = commits.w.year
+                        old_c = commits.c
             workbook.close()
 
 if __name__ == '__main__':
