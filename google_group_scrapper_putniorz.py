9d651db346375b5233bbbbca16cdf252507d8a74
raynald <raynaldchung@gmail.com>
2015-02-16 14:17:00 +0100

    google group

--- google_group_scrapper_putniorz.py
+++ google_group_scrapper_putniorz.py
@@ -2,7 +2,6 @@ import time
 from BeautifulSoup import BeautifulSoup
 from selenium import webdriver
 
-#GOOGLE_GROUP_BASE = 'https://groups.google.com/forum/'
 group_url = 'https://groups.google.com/forum/#!topic/light-table-discussion/5YscXFxTLRE'
 
 def thread_to_dict(thread):
@@ -24,8 +23,28 @@ if __name__ == '__main__':
     source = browser.page_source
     source = source.encode('utf-8')
     frontpage = BeautifulSoup(source)
-    print frontpage.prettify()
+    index = 0
+    user_poll = []
+    combiner = {}
+    for span in frontpage.findAll("span", "HPFAGND-wb-Q HPFAGND-b-Qb"):
+        user_poll.append({'date': span.text.encode('utf-8')})
+    index = 0
+    for span in frontpage.findAll("span", "HPFAGND-P-a"):
+        user_poll[index]['name'] = span.text.encode('utf-8')
+        index += 1
+    index = 0
+    for span in frontpage.findAll("span", "HPFAGND-wb-fb"):
+        user_poll[index]['comment'] = span.text.encode('utf-8').replace(",","~")
+        index += 1
     browser.quit()
-    #frontpage.make_links_absolute(GOOGLE_GROUP_BASE)
-    #html_threads = frontpage.xpath('//div[@role="listitem"]')
-    #threads = (thread_to_dict(thread) for thread in html_threads)
+    for x in user_poll:
+        if x['name'] in combiner:
+            combiner[x['name']] += x['comment']
+        else:
+            combiner[x['name']] = x['comment']
+    with open('yash/googleGroupList.csv', 'a') as f:
+        for x in user_poll:
+            f.write(x['name']+","+x['date']+","+x['comment']+"\n")
+    with open('yash/googleGroupCombine.csv', 'a') as f:
+        for x in combiner:
+            f.write(x + "," + combiner[x] + "\n")
